32ab5c8a97dc4f14a2b2a88ef669a4a789e7ec1d
zlobste <mykola.krainiuk@airslate.com>
2025-10-24 20:57:43 -0700

    Update scanner

--- cmd/scanner/main.go
+++ cmd/scanner/main.go
@@ -5,11 +5,13 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log/slog"
 	"math/rand"
+	"os"
 	"time"
 
 	"cloud.google.com/go/pubsub"
-	"github.com/censys/scan-takehome/pkg/scanning"
+	"github.com/zlobste/mini-scan/pkg/scanning"
 )
 
 var (
@@ -17,20 +19,30 @@ var (
 )
 
 func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
 	projectId := flag.String("project", "test-project", "GCP Project ID")
 	topicId := flag.String("topic", "scan-topic", "GCP PubSub Topic ID")
+	flag.Parse()
+
+	logger.Info("scanner starting", "project", *projectId, "topic", *topicId)
 
 	ctx := context.Background()
 
 	client, err := pubsub.NewClient(ctx, *projectId)
 	if err != nil {
-		panic(err)
+		logger.Error("failed to create pubsub client", "error", err)
+		os.Exit(1)
 	}
+	defer client.Close()
+	logger.Info("pubsub client created")
 
 	topic := client.Topic(*topicId)
 
-	for range time.Tick(time.Second) {
+	logger.Info("publishing scans", "topic", *topicId, "interval", "1s")
 
+	for range time.Tick(time.Second) {
 		scan := &scanning.Scan{
 			Ip:        fmt.Sprintf("1.1.1.%d", rand.Intn(255)),
 			Port:      uint32(rand.Intn(65535)),
@@ -50,12 +62,16 @@ func main() {
 
 		encoded, err := json.Marshal(scan)
 		if err != nil {
-			panic(err)
+			logger.Error("failed to marshal scan", "error", err)
+			continue
 		}
 
 		_, err = topic.Publish(ctx, &pubsub.Message{Data: encoded}).Get(ctx)
 		if err != nil {
-			panic(err)
+			logger.Error("failed to publish message", "error", err)
+			continue
 		}
+
+		logger.Info("scan published", "ip", scan.Ip, "port", scan.Port, "service", scan.Service)
 	}
 }
